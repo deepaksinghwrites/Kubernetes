8557e1545dfd4154dfa510f1e66d457da188e593
unknown <fotografiaartyzmi01@gmail.com>
2024-11-25 23:40:03 +0100

    Added CA yaml

--- EKS/Terraform/backend-resources.tf
+++ EKS/Terraform/backend-resources.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "michael-s3-demo-xyz" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
