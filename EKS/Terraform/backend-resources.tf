cf187ab8854859af6c5e580a6349526e75117669
LaptopDev <fotografiaartyzmi01@gmail.com>
2024-12-08 14:00:40 +0100

    KEDA

--- EKS/Terraform/backend-resources.tf
+++ EKS/Terraform/backend-resources.tf
@@ -1,18 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "michael-s3-demo-xyz" # change this
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#resource "aws_s3_bucket" "s3_bucket" {
+#  bucket = "michael-s3-demo-xyz" # change this
+#}
+#
+#resource "aws_dynamodb_table" "terraform_lock" {
+#  name           = "terraform-lock"
+#  billing_mode   = "PAY_PER_REQUEST"
+#  hash_key       = "LockID"
+#
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
--- EKS/Terraform/backend.tf
+++ EKS/Terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "michael-s3-demo-xyz" # change this
-    key            = "michael/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "michael-s3-demo-xyz" # change this
+#    key            = "michael/terraform.tfstate"
+#    region         = "us-east-1"
+#    encrypt        = true
+#    dynamodb_table = "terraform-lock"
+#  }
+#}
