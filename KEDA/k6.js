d190b623f49a7421b22caac90552bc05e13a61bc
LaptopDev <fotografiaartyzmi01@gmail.com>
2024-12-11 23:45:52 +0100

    Added KEDA dir

---
+++ KEDA/k6.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 50,
+  duration: '30s',
+};
+
+export default function () {
+  http.get('http://dot.13.59.149.2.nip.io');
+  sleep(1);
+}
