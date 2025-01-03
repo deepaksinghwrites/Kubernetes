3dcd5ba2d57766f66618990abe2061236475b552
LaptopDev <fotografiaartyzmi01@gmail.com>
2025-01-03 15:32:56 +0100

    Added bucket var

--- ConifgsAndBackup/EKSCSI/EKS_us_east_1/11-s3-velero.tf
+++ ConifgsAndBackup/EKSCSI/EKS_us_east_1/11-s3-velero.tf
@@ -1,11 +1,12 @@
 # S3 Bucket for Velero Backups
 resource "aws_s3_bucket" "velero" {
-  bucket = "my-velero-bucket-v152"
+  bucket = var.bucket  # Use the bucket variable here
   tags = {
     Name        = "Velero Backup Bucket"
     Environment = "Production"
   }
 }
+
 
 # S3 Bucket Versioning
 resource "aws_s3_bucket_versioning" "velero_versioning" {
--- ConifgsAndBackup/EKSCSI/EKSinit.sh
+++ ConifgsAndBackup/EKSCSI/EKSinit.sh
@@ -2,13 +2,26 @@
 
 set -e  # Exit on error
 
+# Check if the bucket name argument is provided
+if [ -z "$1" ]; then
+  echo "Error: No bucket name provided."
+  echo "Usage: ./EKSinit.sh <bucket_name>"
+  exit 1
+fi
+
+# Assign the first argument to the bucket_name variable
+bucket_name=$1
+
+# Print the bucket name
+echo "Bucket name is: $bucket_name"
+
 # Deploy EKS_1
 echo "Initializing and applying EKS_1..."
 cd EKS_us_east_1
 terraform init
 terraform validate
 terraform plan 
-terraform apply -auto-approve
+terraform apply -var "bucket=$bucket_name" -auto-approve
 # Run terraform output to get the outputs in JSON format
 terraform output -json > terraform_outputs.json
 
